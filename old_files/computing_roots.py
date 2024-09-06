482584552ee079d3f81afa61bda14f91c91cdc7d
Giacomo Pope <giacomopope@gmail.com>
2024-09-06 12:28:23 +0100

    write code for eighth root

--- old_files/computing_roots.py
+++ old_files/computing_roots.py
@@ -76,7 +76,7 @@ def fourth_Fp2(x):
 
     Which together with
 
-    n = y0^2 - y1^2
+    n = y0^2 + y1^2
 
     Allows us to find a quadratic equation in y0^2
 
@@ -128,25 +128,78 @@ def fourth_Fp2(x):
     assert r**4 == x, "Whole thing is broken??"
     return r
 
+def eighth_Fp(x):
+    p = x.parent().characteristic()
+    exp = (p + 1) // 16
+
+    r = x**exp
+    if r**8 != x:
+        raise ValueError("eighth root failed in Fp")
+
+    if int(r) % 2 != 0:
+        return -r
+    return r
+
+def eighth_Fp2(x):
+    """
+    Compute 8th roots in Fp2
+    """
+    def inner_sqrt(A0, B0, N1, check_square=False):
+        
+        C1 = (A0 + N1) / 2
+        if check_square and not C1.is_square():
+            C1 = (A0 - N1) / 2
+        assert C1.is_square(), "C1 must be a square..."
+        A1 = sqrt_Fp(C1)
+        assert A1 * A1 == C1, "sqrt failed..."
+        B1 = B0 / (2*A1)
+        return A1, B1
+
+    F = x.parent()
+    x0, x1 = x.list()
+    delta = x0**2 + x1**2
+    n = eighth_Fp(delta)
+    assert n**8 == delta, "Eighth root didnt work"
+
+    # initialization
+    A0 = x0
+    B0 = x1
+
+    A1, B1 = inner_sqrt(A0, B0, n**4)    
+    A2, B2 = inner_sqrt(A1, B1, n**2)
+    A3, B3 = inner_sqrt(A2, B2, n**1, check_square=True)
+
+    return F([A3, B3])
+
 
 if __name__ == "__main__":
-    # p = 79 * 2**247 - 1
-    # p = 199
-    p = 167
-    assert p % 8 == 7
+    p = 127
+    assert p % 16 == 15
     F = GF(p**2, name="i", modulus=[1, 0, 1])
     i = F.gen()
 
     for b0 in range(p):
         for b1 in range(p):
             b = F([b0, b1])
-            a = b**4
+            a = b**8
+
+            if a[0] == 0 or a[1] == 0:
+                continue
 
             try:
-                x = fourth_Fp2(a)
+                x = eighth_Fp2(a)
             except Exception as e:
-                print(e)
+                print(f"{e = }")
                 print(f"{a = }")
                 print(f"{b = }")
                 continue
-            assert x**4 == a, f"{a = }, {b = }"
+            assert x**8 == a, f"{x = }, {a = }, {b = }"
+
+    # Big prime
+    p = 5*2**248 - 1
+    F = GF(p**2, name="i", modulus=[1, 0, 1])
+    for _ in range(1000):
+        b = F.random_element()
+        a = b**8
+        x = eighth_Fp2(a)
+        assert x**8 == a
